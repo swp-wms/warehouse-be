f3bb8ec0948240c5d20514434e8020a2b62a0549
TheDarkDuck <thefastestmanal@gmail.com>
2025-06-09 17:29:58 +0700

    Backend Parrtner chưa Validate

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const verifyJwt = require('./middlewares/authMiddleware');
 const credentials = require('./middlewares/credentials');
 const corOptions = require('./config/allowedOrigins');
-
+const partner = require('./controllers/partnerController');
 const express = require('express');
 const app = express();
 require('./config/supabaseClient');
@@ -20,6 +20,13 @@ app.use(express.urlencoded({ extended: false }))
 app.get('/', (req, res) => {
     res.send("Hello World!");
 })
+
+// API for partner
+app.post('/partners', partner.createPartner); // addNew
+app.get('/readOnePartner', partner.getOnePartner); //getOne
+app.get('/partners', partner.getAllPartner); //getAll
+app.put('/updatePartner', partner.updatePartner); //update
+
 
 //no verify jwt
 app.use('/login', require('./routers/login'));
