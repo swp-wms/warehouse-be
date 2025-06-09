b4ea356a309e0a0366796f2049bacdbce6631ac9
Lilyhouser <houselily010@gmail.com>
2025-06-09 14:17:10 +0700

    add delivery

--- controllers/deliveryController.js
+++ controllers/deliveryController.js
@@ -33,13 +33,104 @@ const createDeliveryForOrder = async (req, res) => {
     try {
         const { orderId } = req.params;
         const order = (await supabase.from('order').select().eq('id', orderId).single()).data;
-        console.log(order);
 
-        const { deliverydate, deliverytime, gettime, getdate, note } = req.body;
+        if (!order) {
+            return res.status(404).json({ message: 'Không tìm thấy đơn hàng!' });
+        }
+
+        const { deliverydate, deliverytime, gettime, getdate, note, listDeliveryDetail } = req.body;
         if (!deliverydate || !deliverytime) {
             return res.status(400).json({ message: 'Ngày vận chuyển và thời gian vận chuyển là bắt buộc!' });
         }
-        await supabase.from('delivery').insert({ orderid: order.id, deliverydate, deliverytime, gettime, getdate, note });
+        if (listDeliveryDetail === null || listDeliveryDetail.length < 1) {
+            return res.status(400).json({ message: 'Một xe hàng không thể để trống!' });
+        }
+
+        const delivery = await supabase.from('delivery').insert({ orderid: order.id, deliverydate, deliverytime, gettime, getdate, note, deliverystatus: 'Chờ gán xe' }).select();
+
+        const deliveryid = delivery.data[0].id;
+
+        listDeliveryDetail.map(item => {
+            item.deliveryid = deliveryid;
+        });
+
+        await supabase.from('deliverydetail').insert(listDeliveryDetail);
+
+        res.sendStatus(200);
+    } catch (error) {
+        res.sendStatus(500);
+    }
+}
+
+// const updateDeliveryList = async (req, res) => {
+//     try {
+//         const { deliveryId } = req.params;
+//         const delivery = (await supabase.from('delivery').select().eq('id', deliveryId).single()).data;
+        
+//         const { drivername, drivercode, driverphonenumber, licenseplate } = req.body;
+//         if (!drivername || !drivercode || !driverphonenumber || !licenseplate) {
+//             return res.status(400).json({ message: 'Vui lòng điền đủ thông tin tài xế và xe!' });
+//         }
+//         await supabase.from('delivery').update(
+//             {
+//                 drivername,
+//                 drivercode,
+//                 driverphonenumber,
+//                 licenseplate,
+//                 deliverystatus: 'Chờ duyệt'
+//             }).eq('id', deliveryId);
+
+//         res.sendStatus(200);
+//     } catch (error) {
+//         res.sendStatus(500);
+//     }
+// }
+
+const addTruckForDelivery = async (req, res) => {
+    try {
+        const { deliveryId } = req.params;
+        const delivery = (await supabase.from('delivery').select().eq('id', deliveryId).single()).data;
+        
+        if(!delivery) {
+            return res.status(404).json({
+                message: 'Không tìm thấy giao hàng!'
+            })
+        }
+        const { drivername, drivercode, driverphonenumber, licenseplate } = req.body;
+        if (!drivername || !drivercode || !driverphonenumber || !licenseplate) {
+            return res.status(400).json({ message: 'Vui lòng điền đủ thông tin tài xế và xe!' });
+        }
+        await supabase.from('delivery').update(
+            {
+                drivername,
+                drivercode,
+                driverphonenumber,
+                licenseplate,
+                deliverystatus: 'Chờ duyệt'
+            }).eq('id', deliveryId);
+
+        res.sendStatus(200);
+    } catch (error) {
+        res.sendStatus(500);
+    }
+}
+
+const approveDelivery = async (req, res) => {
+    try {
+        const { deliveryId } = req.params;
+        const delivery = (await supabase.from('delivery').select().eq('id', deliveryId).single()).data;
+
+        if (!delivery) {
+            return res.sendStatus(404);
+        }
+
+        if (delivery.deliverystatus !== 'Chờ duyệt') {
+            return res.sendStatus(400);
+        }
+
+        const { deliverystatus } = req.body;
+
+        await supabase.from('delivery').update({ 'deliverystatus': deliverystatus ? "Chờ" : "Từ chối xe" }).eq('id', deliveryId);
 
         res.sendStatus(200);
     } catch (error) {
@@ -48,5 +139,9 @@ const createDeliveryForOrder = async (req, res) => {
 }
 
 module.exports = {
-    getOneDelivery, getDeliveryByOrder, createDeliveryForOrder
+    getOneDelivery,
+    getDeliveryByOrder,
+    createDeliveryForOrder,
+    addTruckForDelivery,
+    approveDelivery
 }
