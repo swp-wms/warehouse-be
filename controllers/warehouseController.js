868347979a7b690eb2edf851774ea5342ee3a2b0
ngocmaii00 <mailyngoc2301@gmail.com>
2025-06-27 12:14:53 +0700

    add:future-weight

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -1,31 +1,47 @@
 const supabase = require("../config/supabaseClient");
 
 const totalWeightofWH = async (req, res) => {
-    try {
-        const totalweight = (await supabase.from('totalweightwh').select('')).data;
-        res.json(totalweight);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-}
+  try {
+    const totalweight = (await supabase.from("totalweightwh").select("")).data;
+    res.json(totalweight);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const totalFutureWeightofWH = async (req, res) => {
+  try {
+    const { createdate } = req.params;
+    const weight = (await supabase.rpc("get_weight_by_date", { delivery_date: createdate })).data[0];
+    const totalweight = (await supabase.from("totalweightwh").select("")).data;
+    const totalFuture = totalweight[0].sum - weight.total_weight_by_date;
+    const percent_future = totalFuture/15000000*100; 
+    res.send({ 
+      totalFuture,
+      percent_future
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
 
 const sumImport = async (req, res) => {
-    try {
-        const sumImport = (await supabase.from('sumimport').select('')).data;
-        res.json(sumImport);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-}
+  try {
+    const sumImport = (await supabase.from("sumimport").select("")).data;
+    res.json(sumImport);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 const sumExport = async (req, res) => {
-    try {
-        const sumExport = (await supabase.from('sumexport').select('')).data;
-        res.json(sumExport);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-}
+  try {
+    const sumExport = (await supabase.from("sumexport").select("")).data;
+    res.json(sumExport);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
-
-module.exports = { totalWeightofWH, sumImport, sumExport };
+module.exports = { totalWeightofWH, totalFutureWeightofWH, sumImport, sumExport };
--- routers/api/warehouse.js
+++ routers/api/warehouse.js
@@ -1,13 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const verifyRoles = require('../../middlewares/roleMiddleware');
-const role = require('../../data/role');
-const { totalWeightofWH, sumExport, sumImport } = require('../../controllers/warehouseController');
+const verifyRoles = require("../../middlewares/roleMiddleware");
+const role = require("../../data/role");
+const {
+  totalWeightofWH,
+  sumExport,
+  sumImport,
+  totalFutureWeightofWH
+} = require("../../controllers/warehouseController");
 
-router.route('/').get(verifyRoles(role.WAREHOUSE_KEEPER, role.SALESMAN),totalWeightofWH)
+router
+  .route("/")
+  .get(verifyRoles(role.WAREHOUSE_KEEPER, role.SALESMAN), totalWeightofWH);
 
-router.route('/import').get(verifyRoles(role.WAREHOUSE_KEEPER, role.SALESMAN),sumImport)
+router
+  .route("/import")
+  .get(verifyRoles(role.WAREHOUSE_KEEPER, role.SALESMAN), sumImport);
 
-router.route('/export').get(verifyRoles(role.WAREHOUSE_KEEPER, role.SALESMAN),sumExport)
+router
+  .route("/export")
+  .get(verifyRoles(role.WAREHOUSE_KEEPER, role.SALESMAN), sumExport);
+
+  router.route("/future/:createdate")
+  .get(verifyRoles(role.WAREHOUSE_KEEPER, role.SALESMAN), totalFutureWeightofWH);
 
 module.exports = router;
