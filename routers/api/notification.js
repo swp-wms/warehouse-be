3c437fb4b1d5d2d4a371080ad4fc73e657668de2
Lilyhouser <houselily010@gmail.com>
2025-07-15 00:43:59 +0700

    add: load seen notifs

--- routers/api/notification.js
+++ routers/api/notification.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const {
-    getAllNotification, seenNotification
+    getAllNotification, seenNotification, getSeenNotification
 } = require('../../controllers/notificationController');
 
 router.get('', getAllNotification);
+router.get('/seen/:index', getSeenNotification);
 router.post('/:id', seenNotification);
 
 module.exports = router;
